d130631bb2158f5cf556b1a65fad225772500843
Toly <anatoly.kvitnitsky@gmail.com>
2015-02-11 22:55:36 +0000

    add view items and tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+ resources :todo_lists do
+    resources :todo_items
+  end
   root 'todo_lists#index'
-  
-  resources :todo_lists
-  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
